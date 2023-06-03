4f41e6fdbe72c9997ab3e866364abea67de5b67b
chenshen <scv119@gmail.com>
2023-06-02 23:54:56 -0700

    support pass by pointers

--- vector-add.py
+++ vector-add.py
@@ -4,8 +4,8 @@ import triton
 import triton.language as tl
 
 @triton.jit
-def add_kernel(
-    x_ptr,
+def add_kernel1(
+    look_up_ptr,
     y_ptr,
     output_ptr,
     n_elements,
@@ -18,7 +18,8 @@ def add_kernel(
 
     mask = offsets < n_elements
 
-    x = tl.load(x_ptr + block_start + offsets, mask=mask)
+    x_ptr = tl.load(look_up_ptr).to(tl.pointer_type(tl.float32))
+    x = tl.load(x_ptr+ block_start + offsets, mask=mask)
     y = tl.load(y_ptr + block_start + offsets, mask=mask)
 
     output = x + y
@@ -32,39 +33,20 @@ def add(x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
 
     n_elements = output.numel()
 
+    #look_up_tensor = torch.tensor((x.data_ptr() - anchor.data_ptr()) / 4, dtype=torch.int64, device='cuda')
+    look_up_tensor = torch.tensor(x.data_ptr(), dtype=torch.int64, device='cuda')
+
     grid = lambda meta: (triton.cdiv(n_elements, meta['BLOCK_SIZE']), )
 
-    add_kernel[grid](x, y, output, n_elements, BLOCK_SIZE=1024)
+    add_kernel1[grid](look_up_tensor, y, output, n_elements, BLOCK_SIZE=1024)
 
     return output
 
 
-@triton.jit
-def add_kernel(
-    x_ptr,
-    y_ptr,
-    output_ptr,
-    n_elements,
-    BLOCK_SIZE: tl.constexpr,
-):
-    pid = tl.program_id(axis=0)
-
-    block_start = pid * BLOCK_SIZE
-    offsets = tl.arange(0, BLOCK_SIZE)
-
-    mask = offsets < n_elements
-
-    x = tl.load(x_ptr + block_start + offsets, mask=mask)
-    y = tl.load(y_ptr + block_start + offsets, mask=mask)
-
-    output = x + y
-
-    tl.store(output_ptr + block_start + offsets, output, mask=mask)
-
 torch.manual_seed(0)
 size = 98432
-x = torch.rand(size, device='cuda')
-y = torch.rand(size, device='cuda')
+x = torch.rand(size, device='cuda', dtype=torch.float32)
+y = torch.rand(size, device='cuda', dtype=torch.float32)
 output_torch = x + y
 output_triton = add(x, y)
 print(output_torch)
